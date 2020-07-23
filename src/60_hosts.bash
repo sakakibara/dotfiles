c20cb7596af527004bef80ed664d7f05ab035ecb
Sho Sakakibara <sho@sakakibara.dev>
2020-07-24 01:23:23 +0900

    Make hosts update OS specific

--- src/60_hosts.bash
+++ src/60_hosts.bash
@@ -1,6 +1,8 @@
 # hosts
 # shellcheck disable=SC2024
-if ! grep -Fxq "# Custom host records are listed here." /etc/hosts; then
-  msg_heading "Adding custom host records to the hosts file"
-  sudo tee -a /etc/hosts < "${DOTFILES}/etc/macos/hosts" >/dev/null
+if [[ -f "${DOTFILES}/etc/${OSNAME}/hosts" ]]; then
+  if ! grep -Fxq "# Custom host records are listed here." /etc/hosts; then
+    msg_heading "Adding custom host records to the hosts file"
+    sudo tee -a /etc/hosts < "${DOTFILES}/etc/${OSNAME}/hosts" >/dev/null
+  fi
 fi
