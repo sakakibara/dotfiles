ed83aa4cf02b0aaff5a66842acd11107112490b5
Sho Sakakibara <sho@sakakibara.dev>
2022-12-26 19:23:16 +0900

    Disable pangu-spacing by default

--- dot_doom.d/modules/input/japanese/packages.el
+++ dot_doom.d/modules/input/japanese/packages.el
@@ -8,4 +8,4 @@
     :pin "88738a6657dd3e25ee3ef814512c402505da8669"))
 (when IS-WINDOWS
   (package! tr-ime :pin "87f0677220b755f947fe5f373b6a34e1afb82f3c"))
-(package! pangu-spacing :pin "f92898949ba3bf991fd229416f3bbb54e9c6c223")
+(package! pangu-spacing :pin "f92898949ba3bf991fd229416f3bbb54e9c6c223" :disable t)
