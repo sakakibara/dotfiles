8dd2a761e596f138f641968f314cafb9d1bca1e1
Sho Sakakibara <sho@sakakibara.dev>
2023-03-21 20:30:04 +0900

    Fix global key-binding for chezmoi

--- dot_config/doom/modules/tools/chezmoi/autoload/map.el
+++ dot_config/doom/modules/tools/chezmoi/autoload/map.el
@@ -4,6 +4,6 @@
 (defun +chezmoi--init-h ()
   "Initialize chezmoi"
   (map! :leader
-        :prefix "f"
-        :desc "Find file in dotfiles" "t" #'chezmoi-find
-        :desc "Browse dotfiles" "T" #'+chezmoi--browse-dotfiles))
+        (:prefix "f"
+         :desc "Find file in dotfiles" "t" #'chezmoi-find
+         :desc "Browse dotfiles" "T" #'+chezmoi--browse-dotfiles)))
