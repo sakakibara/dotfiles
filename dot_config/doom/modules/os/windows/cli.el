a1ca548c9329a748d22bbced34bb61251e11f61c
Sho Sakakibara <sho@sakakibara.dev>
2023-03-22 21:09:36 +0900

    Workaround to avoid cmd.exe 8191 character limit

--- dot_config/doom/modules/os/windows/cli.el
+++ dot_config/doom/modules/os/windows/cli.el
@@ -1,5 +1,7 @@
 ;;; os/windows/cli.el -*- lexical-binding: t; -*-
 
+(setq straight-base-dir "c:/.local/")
+
 (add-to-list 'doom-print-class-alist
              '(success . (lambda (str &rest args)
                  (apply #'doom-print--style 'green
