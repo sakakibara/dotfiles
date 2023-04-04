141e7a0793bb7fdbf3e27f7262ddc6345e805580
Sho Sakakibara <sho@sakakibara.dev>
2023-04-04 18:24:18 +0900

    Update ejc-sql package

--- dot_config/doom/modules/lang/sql/packages.el
+++ dot_config/doom/modules/lang/sql/packages.el
@@ -5,4 +5,4 @@
   :recipe (:host github :repo "alex-hhh/emacs-sql-indent")
   :pin "5a3ce9e52cc3fb55ec02f3a38f43d687a9e3672d")
 (package! sqlup-mode :pin "3f9df9c88d6a7f9b1ae907e401cad8d3d7d63bbf")
-(package! ejc-sql :pin "5e100426be90fc193154b1219ab1058534a4fb52")
+(package! ejc-sql :pin "835de657908a822c80ac30ee085c59f8f9974557")
