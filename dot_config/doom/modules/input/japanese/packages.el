2744c10f7dd0fc7bd5699975359ce126a16383be
Sho Sakakibara <sho@sakakibara.dev>
2023-04-04 18:22:59 +0900

    Update tr-ime package

--- dot_config/doom/modules/input/japanese/packages.el
+++ dot_config/doom/modules/input/japanese/packages.el
@@ -7,5 +7,5 @@
     :recipe (:host github :repo "tam17aki/avy-migemo")
     :pin "88738a6657dd3e25ee3ef814512c402505da8669"))
 (when IS-WINDOWS
-  (package! tr-ime :pin "87f0677220b755f947fe5f373b6a34e1afb82f3c"))
+  (package! tr-ime :pin "063aee5687a62c583e084d56f2b60b673961c8c9"))
 (package! pangu-spacing :pin "f92898949ba3bf991fd229416f3bbb54e9c6c223" :disable t)
