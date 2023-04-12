15717d33e2bc0f424ee77fb82db444b5e6759dfd
Sho Sakakibara <sho@sakakibara.dev>
2023-04-12 15:18:25 +0900

    Make mini.surround behave like vim-surround

--- dot_config/nvim/lua/plugins/coding.lua
+++ dot_config/nvim/lua/plugins/coding.lua
@@ -2,8 +2,28 @@ return {
   {
     "echasnovski/mini.surround",
     event = "VeryLazy",
+    opts = {
+      mappings = {
+        add = 'ys',
+        delete = 'ds',
+        find = '',
+        find_left = '',
+        highlight = '',
+        replace = 'cs',
+        update_n_lines = '',
+        suffix_last = '',
+        suffix_next = '',
+      },
+      search_method = 'cover_or_next',
+    },
     config = function(_, opts)
       require("mini.surround").setup(opts)
+      -- Remap adding surrounding to Visual mode selection
+      vim.keymap.del('x', 'ys')
+      vim.keymap.set('x', 'S', [[:<C-u>lua MiniSurround.add('visual')<CR>]], { silent = true })
+
+      -- Make special mapping for "add surrounding for line"
+      vim.keymap.set('n', 'yss', 'ys_', { remap = true })
     end,
   },
 
