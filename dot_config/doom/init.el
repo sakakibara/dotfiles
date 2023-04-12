cbaece128506ec5f8773014b072918afea8bcfb9
Sho Sakakibara <sho@sakakibara.dev>
2023-04-12 09:14:46 +0900

    Add poetry package

--- dot_config/doom/init.el
+++ dot_config/doom/init.el
@@ -172,7 +172,8 @@
        plantuml            ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        (python +lsp
-               +pyright)   ; beautiful is better than ugly
+               +pyright
+               +poetry)    ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
