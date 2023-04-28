3989824da81327d975efdf2505878f609b032a9c
Sho Sakakibara <sho@sakakibara.dev>
2023-04-28 20:17:36 +0900

    Add encoding setting for ripgrep process

--- dot_config/doom/modules/os/windows/config.el
+++ dot_config/doom/modules/os/windows/config.el
@@ -20,6 +20,9 @@
                                '((depth . 99)))))
 
 (modify-coding-system-alist 'file "\\.sql\\'" 'cp932-dos)
+
+(add-to-list 'process-coding-system-alist '("[rR][gG]" utf-8-dos . utf-8-unix))
+
 (setq doom-leader-alt-key "C-M-SPC"
       doom-localleader-alt-key "C-M-SPC m")
 
