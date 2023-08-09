7e5785d2265b801fe007a694990eb4038d409d17
Sho Sakakibara <sho@sakakibara.dev>
2023-08-10 01:32:18 +0900

    Add which-key

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -1,5 +1,23 @@
 return {
   {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      plugins = { spelling = true },
+      defaults = {
+        mode = { "n", "v" },
+        ["<leader>b"] = { name = "+buffer" },
+        ["<leader>f"] = { name = "+file" },
+      },
+    },
+    config = function(_, opts)
+      local wk = require("which-key")
+      wk.setup(opts)
+      wk.register(opts.defaults)
+    end,
+  },
+
+  {
     "echasnovski/mini.basics",
     config = function(_, opts)
       require("mini.basics").setup(opts)
