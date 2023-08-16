cbd190703b3a73f6df377da6ea4676987cd8d353
Sho Sakakibara <sho@sakakibara.dev>
2023-08-17 00:06:01 +0900

    Change flash.nvim to have sneak like mappings

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -129,9 +129,9 @@ return {
     event = "VeryLazy",
     opts = {},
     keys = {
-      { "s", mode = { "n" }, function() require("flash").jump() end, desc = "Flash" },
+      { "s", mode = { "n", "x" }, function() require("flash").jump() end, desc = "Flash" },
       { "S", mode = { "n" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-      { "z", mode = { "o", "x" }, function() require("flash").jump() end, desc = "Flash" },
+      { "z", mode = { "o" }, function() require("flash").jump() end, desc = "Flash" },
       { "Z", mode = { "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
       { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
       { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
