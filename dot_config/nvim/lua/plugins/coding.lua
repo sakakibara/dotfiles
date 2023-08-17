1912b4191e3bcb2c1399cdfa50a6dfbfb14a79f5
Sho Sakakibara <sho@sakakibara.dev>
2023-08-18 07:40:53 +0900

    Add icons to nvim-cmp completion menu

--- dot_config/nvim/lua/plugins/coding.lua
+++ dot_config/nvim/lua/plugins/coding.lua
@@ -153,6 +153,15 @@ return {
           { name = "buffer" },
           { name = "path" },
         }),
+        formatting = {
+          format = function(_, item)
+            local icons = require("config.icons").kinds
+            if icons[item.kind] then
+              item.kind = icons[item.kind] .. item.kind
+            end
+            return item
+          end,
+        },
         experimental = {
           ghost_text = {
             hl_group = "LspCodeLens",
