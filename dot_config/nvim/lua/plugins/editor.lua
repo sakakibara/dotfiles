806735c098d402352e711e673721a02ab89a3975
Sho Sakakibara <sho@sakakibara.dev>
2023-08-22 20:38:27 +0900

    Move telescope config to editor

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -1,3 +1,5 @@
+local Util = require("util")
+
 return {
   {
     "folke/which-key.nvim",
@@ -235,6 +237,126 @@ return {
     keys = {
       { "<leader>sr", function() require("spectre").open() end, desc = "Spectre" },
     },
+  },
+
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-live-grep-args.nvim",
+      "nvim-telescope/telescope-file-browser.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+      },
+    },
+    cmd = "Telescope",
+    version = false,
+    keys = {
+      { "<leader><space>", Util.telescope("files", { cwd = false }), desc = "Find Files (cwd)" },
+      { "<leader>bb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+      { "<leader>ff", Util.telescope("file_browser"), desc = "Find Files" },
+      { "<leader>fF", Util.telescope("files"), desc = "Find Files (root dir)" },
+      { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
+      { "<leader>fR", Util.telescope("oldfiles", { cwd = vim.loop.cwd() }), desc = "Recent (cwd)" },
+      { '<leader>s"', "<cmd>Telescope registers<cr>", desc = "Registers" },
+      { "<leader>ss", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer" },
+      { "<leader>sc", "<cmd>Telescope command_history<cr>", desc = "Command History" },
+      { "<leader>sg", Util.telescope("live_grep"), desc = "Grep (root dir)" },
+      { "<leader>sG", Util.telescope("live_grep", { cwd = false }), desc = "Grep (cwd)" },
+      { "<leader>sw", Util.telescope("grep_string", { word_match = "-w" }), desc = "Word (root dir)" },
+      { "<leader>sW", Util.telescope("grep_string", { cwd = false, word_match = "-w" }), desc = "Word (cwd)" },
+      { "<leader>sw", Util.telescope("grep_string"), mode = "v", desc = "Selection (root dir)" },
+      { "<leader>sW", Util.telescope("grep_string", { cwd = false }), mode = "v", desc = "Selection (cwd)" },
+      { "<leader>ht", "<cmd>Telescope help_tags<cr>", desc = "Help Tags" },
+      { "<leader>hk", "<cmd>Telescope keymaps<cr>", desc = "Keymaps" },
+      { "<leader>uc", Util.telescope("colorscheme", { enable_preview = true }), desc = "Colorschemes" },
+      { "<leader>:", "<cmd>Telescope commands<cr>", desc = "Commands" },
+      { "<leader>/", Util.telescope("live_grep_args", { cwd = false }), desc = "Live Grep Args (cwd)" },
+    },
+    opts = {
+      defaults = {
+        preview = { hide_on_startup = true },
+        prompt_prefix = "❯ ",
+        selection_caret = " ",
+        mappings = {
+          i = {
+            ["<c-t>"] = function(...)
+              return require("trouble.providers.telescope").open_with_trouble(...)
+            end,
+            ["<a-i>"] = function()
+              local action_state = require("telescope.actions.state")
+              local line = action_state.get_current_line()
+              Util.telescope("find_files", { no_ignore = true, default_text = line })()
+            end,
+            ["<a-h>"] = function()
+              local action_state = require("telescope.actions.state")
+              local line = action_state.get_current_line()
+              Util.telescope("find_files", { hidden = true, default_text = line })()
+            end,
+            ["<C-Down>"] = function(...)
+              return require("telescope.actions").cycle_history_next(...)
+            end,
+            ["<C-Up>"] = function(...)
+              return require("telescope.actions").cycle_history_prev(...)
+            end,
+            ["<C-j>"] = function(...)
+              return require("telescope.actions").move_selection_next(...)
+            end,
+            ["<C-k>"] = function(...)
+              return require("telescope.actions").move_selection_previous(...)
+            end,
+            ["<C-n>"] = function(...)
+              return require("telescope.actions").preview_scrolling_down(...)
+            end,
+            ["<C-p>"] = function(...)
+              return require("telescope.actions").preview_scrolling_up(...)
+            end,
+            ["<Esc>"] = function(...)
+              return require("telescope.actions").close(...)
+            end,
+            ["jk"] = function(...)
+              return require("telescope.actions").close(...)
+            end,
+            ["<M-p>"] = function(...)
+              return require("telescope.actions.layout").toggle_preview(...)
+            end,
+          },
+          n = {
+            ["q"] = function(...)
+              return require("telescope.actions").close(...)
+            end,
+            ["<M-p>"] = function(...)
+              return require("telescope.actions.layout").toggle_preview(...)
+            end,
+          },
+        },
+      },
+      extensions = {
+        file_browser = {
+          theme = "ivy",
+          mappings = {
+            i = {
+              ["<Tab>"] = function(...)
+                return require("telescope.actions").select_default(...)
+              end,
+            },
+          },
+        },
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
+          override_file_sorter = true,
+          case_mode = "smart_case",
+        },
+      },
+    },
+    config = function(_, opts)
+      require("telescope").setup(opts)
+      require("telescope").load_extension("live_grep_args")
+      require("telescope").load_extension("file_browser")
+      require("telescope").load_extension("fzf")
+    end
   },
 
   {
