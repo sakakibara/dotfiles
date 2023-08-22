1f0068c1a1a06dbee259a34303eeb2c6fe081485
Sho Sakakibara <sho@sakakibara.dev>
2023-08-22 18:37:24 +0900

    Disable flash.nvim for regular search

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -147,7 +147,13 @@ return {
   {
     "folke/flash.nvim",
     event = "VeryLazy",
-    opts = {},
+    opts = {
+      modes = {
+        search = {
+          enabled = false,
+        },
+      },
+    },
     keys = {
       { "s", mode = { "n", "x" }, function() require("flash").jump() end, desc = "Flash" },
       { "S", mode = { "n" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
