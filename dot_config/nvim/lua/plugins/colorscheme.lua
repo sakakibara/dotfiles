398cdcf6f26ffb711b89c064739b87512e61a3ed
Sho Sakakibara <sho@sakakibara.dev>
2023-08-22 21:07:10 +0900

    Change catppuccin theme settings

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -3,7 +3,24 @@ return {
   lazy = false,
   priority = 1000,
   name = "catppuccin",
-  opts = { flavour = "mocha" },
+  opts = {
+    flavour = jit.os:find("Linux") and "latte" or "mocha",
+    integrations = {
+      cmp = true,
+      flash = true,
+      gitsigns = true,
+      illuminate = true,
+      indent_blankline = {enabled = true },
+      markdown = true,
+      mason = true,
+      navic = { enabled = true, custom_bg = "lualine" },
+      neotree = true,
+      notify = true,
+      telescope = true,
+      treesitter = true,
+      which_key = true,
+    }
+  },
   config = function(_, opts)
     require("catppuccin").setup(opts)
     vim.cmd.colorscheme("catppuccin")
