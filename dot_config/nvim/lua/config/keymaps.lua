6676338bed018b930fac38ece5d00ea4b6ffa604
Sho Sakakibara <sho@sakakibara.dev>
2023-08-30 00:45:47 +0900

    Add utility mappings

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,25 @@
 local map = vim.keymap.set
+
 map("i", "jk", "<Esc>")
 map("n", "<Leader>w", "<C-w>")
+
+map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+
+map({ "n", "x", "o" }, "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
+map({ "n", "x", "o" }, "N", "'nN'[v:searchforward]", { expr = true, desc = "Previous search result" })
+
 map("n", "<Leader>bn", "<cmd>bnext<cr>", { desc = "Next buffer" })
 map("n", "<Leader>bp", "<cmd>bNext<cr>", { desc = "Previous buffer" })
 map("n", "<Leader>bN", "<cmd>enew<cr>", { desc = "New empty buffer" })
+map("n", "<Leader>bs", "<cmd>w<cr>", { desc = "Save buffer" })
+
+map("n", "<leader>xl", "<cmd>lopen<cr>", { desc = "Location list" })
+map("n", "<leader>xq", "<cmd>copen<cr>", { desc = "Quickfix list" })
+
+map("n", "<leader>[q", "<cmd>cprevious<cr>", { desc = "Previous quickfix" })
+map("n", "<leader>]q", "<cmd>cnext<cr>", { desc = "Next quickfix" })
+
+map("n", "<leader>ul", "<cmd>Lazy<cr>", { desc = "Lazy" })
+
+map("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit all" })
