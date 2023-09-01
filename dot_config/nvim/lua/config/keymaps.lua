c5f21920a5209611764ad535ec940d722e71a07a
Sho Sakakibara <sho@sakakibara.dev>
2023-09-01 19:39:03 +0900

    Make all leader prefix lowercase

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -2,7 +2,7 @@ local map = vim.keymap.set
 
 map("i", "jk", "<Esc>")
 map("t", "jk", "<C-\\><C-n>")
-map("n", "<Leader>w", "<C-w>")
+map("n", "<leader>w", "<C-w>")
 
 map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
@@ -10,10 +10,10 @@ map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true
 map({ "n", "x", "o" }, "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
 map({ "n", "x", "o" }, "N", "'nN'[v:searchforward]", { expr = true, desc = "Previous search result" })
 
-map("n", "<Leader>bn", "<cmd>bnext<cr>", { desc = "Next buffer" })
-map("n", "<Leader>bp", "<cmd>bNext<cr>", { desc = "Previous buffer" })
-map("n", "<Leader>bN", "<cmd>enew<cr>", { desc = "New empty buffer" })
-map("n", "<Leader>bs", "<cmd>w<cr>", { desc = "Save buffer" })
+map("n", "<leader>bn", "<cmd>bnext<cr>", { desc = "Next buffer" })
+map("n", "<leader>bp", "<cmd>bNext<cr>", { desc = "Previous buffer" })
+map("n", "<leader>bN", "<cmd>enew<cr>", { desc = "New empty buffer" })
+map("n", "<leader>bs", "<cmd>w<cr>", { desc = "Save buffer" })
 
 map("n", "<leader>xl", "<cmd>lopen<cr>", { desc = "Location list" })
 map("n", "<leader>xq", "<cmd>copen<cr>", { desc = "Quickfix list" })
