4d5dbe4db53d996ecb418ad3c53c8ac6934c7a28
Sho Sakakibara <sho@sakakibara.dev>
2023-09-08 15:52:20 +0900

    Change lualine filename symbols setting

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -32,7 +32,7 @@ return {
           lualine_c = {
             "diagnostics",
             { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
-            { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
+            { "filename", path = 1 },
           },
           lualine_x = {
             "encoding",
