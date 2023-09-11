d4d28b74364230d5a68e30026f557b478c7db7f3
Sho Sakakibara <sho@sakakibara.dev>
2023-09-12 00:29:15 +0900

    Add overseer.nvim

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -201,6 +201,41 @@ return {
   },
 
   {
+    "stevearc/overseer.nvim",
+    cmd = {
+      "OverseerOpen",
+      "OverseerClose",
+      "OverseerToggle",
+      "Grep",
+    },
+    opts = {},
+    config = function(_, opts)
+      require("overseer").setup(opts)
+      vim.api.nvim_create_user_command("Grep", function(params)
+        local cmd, num_subs = vim.o.grepprg:gsub("%$%*", params.args)
+        if num_subs == 0 then
+          cmd = cmd .. " " .. params.args
+        end
+        local task = require("overseer").new_task({
+          cmd = vim.fn.expandcmd(cmd),
+          components = {
+            {
+              "on_output_quickfix",
+              errorformat = vim.o.grepformat,
+              open = not params.bang,
+              open_height = 8,
+              items_only = true,
+            },
+            { "on_complete_dispose", timeout = 30 },
+            "default",
+          },
+        })
+        task:start()
+      end, { nargs = "*", bang = true, complete = "file" })
+    end
+  },
+
+  {
     "RRethy/vim-illuminate",
     event = "BufReadPost",
     opts = { delay = 200 },
