ba21d0e9a731a26939c73d5ec27dc7d029a971cd
Sho Sakakibara <sho@sakakibara.dev>
2023-09-12 21:07:42 +0900

    Add hydra.nvim

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -226,6 +226,25 @@ return {
   },
 
   {
+    "anuvyklack/hydra.nvim",
+    event = "VeryLazy",
+    config = function()
+      local Hydra = require("hydra")
+      Hydra({
+        name = "Side scroll",
+        mode = "n",
+        body = "z",
+        heads = {
+          { "h", "5zh" },
+          { "l", "5zl", { desc = "←/→" } },
+          { "H", "zH" },
+          { "L", "zL", { desc = "half screen ←/→" } },
+        }
+      })
+    end,
+  },
+
+  {
     "stevearc/oil.nvim",
     keys = {
       { "-", function() require("oil").open() end, desc = "Open parent directory" },
