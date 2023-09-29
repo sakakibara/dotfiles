738b8f74216c40deba55048e28445ec5e136f121
Sho Sakakibara <sho@sakakibara.dev>
2023-09-29 18:22:49 +0900

    Clear default aliases of nvim-surround

--- dot_config/nvim/lua/plugins/coding.lua
+++ dot_config/nvim/lua/plugins/coding.lua
@@ -3,9 +3,9 @@ return {
     "kylechui/nvim-surround",
     version = "*",
     event = "VeryLazy",
-    config = function()
-      require("nvim-surround").setup()
-    end
+    opts = {
+      aliases = {},
+    },
   },
 
   {
