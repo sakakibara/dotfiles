65bbb0f46b03f9d780c8f01df7f0314c2ee304fc
Sho Sakakibara <sho@sakakibara.dev>
2023-10-01 23:20:36 +0900

    Add c treesitter parser

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -23,6 +23,7 @@ return {
       indent = { enable = true },
       context_commentstring = { enable = true, enable_autocmd = false },
       ensure_installed = {
+        "c",
         "bash",
         "css",
         "csv",
