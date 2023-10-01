3f5d0e6685b436199999b6e5e7148c65348e30b0
Sho Sakakibara <sho@sakakibara.dev>
2023-10-02 01:47:28 +0900

    Add new zk journal note mapping

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -694,6 +694,7 @@ return {
     "mickael-menu/zk-nvim",
     keys = {
       { "<leader>nn", "<cmd>ZkNew { title = vim.fn.input('Title: ') }<cr>", desc = "Create zk note" },
+      { "<leader>nj", "<cmd>ZkNew { group = 'journal' }<cr>", desc = "Create zk journal note" },
       { "<leader>nf", "<cmd>ZkNotes { sort = { 'modified' } }<cr>", desc = "Open zk note" },
       { "<leader>ns", "<cmd>ZkGrep<cr>", desc = "Search zk note" },
       { "<leader>nt", "<cmd>ZkTags<cr>", desc = "Open zk note by tags" },
