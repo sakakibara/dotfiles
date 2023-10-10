1a08cda3a4a5a158a8895a31d89944827490222c
Sho Sakakibara <sho@sakakibara.dev>
2023-10-10 17:44:31 +0900

    Change heirline load event to UIEnter

--- dot_config/nvim/lua/plugins/heirline.lua
+++ dot_config/nvim/lua/plugins/heirline.lua
@@ -1,7 +1,7 @@
 return {
   {
     "rebelot/heirline.nvim",
-    event = "VeryLazy",
+    event = "UIEnter",
     config = function()
       local conditions = require("heirline.conditions")
       local utils = require("heirline.utils")
