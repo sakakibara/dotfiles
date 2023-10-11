f9fb6b82eb935f55d3cc6d4ada1cb5e21852923b
Sho Sakakibara <sho@sakakibara.dev>
2023-10-12 00:20:18 +0900

    Remove unused function

--- dot_config/nvim/lua/util/plugin.lua
+++ dot_config/nvim/lua/util/plugin.lua
@@ -1,20 +1,5 @@
 local M = {}
 
-function M.bootstrap()
-  local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-  if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-      "git",
-      "clone",
-      "--filter=blob:none",
-      "https://github.com/folke/lazy.nvim.git",
-      "--branch=stable",
-      lazypath,
-    })
-  end
-  vim.opt.rtp:prepend(lazypath)
-end
-
 M.use_lazy_file = true
 M.lazy_file_events = { "BufReadPost", "BufNewFile", "BufWritePre" }
 
