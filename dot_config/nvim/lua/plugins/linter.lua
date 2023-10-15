c293c4cc9a9adcc312fcfa2f89a03b9617a847eb
Sho Sakakibara <sho@sakakibara.dev>
2023-10-15 19:07:49 +0900

    Add fallback linter settings

--- dot_config/nvim/lua/plugins/linter.lua
+++ dot_config/nvim/lua/plugins/linter.lua
@@ -16,11 +16,12 @@ return {
       },
     },
     config = function(_, opts)
+      local LazyUtil = require("lazy.core.util")
       local M = {}
 
       local lint = require("lint")
       for name, linter in pairs(opts.linters) do
-        if type(linter) == "table" and type(lint.linters) == "table" then
+        if type(linter) == "table" and type(lint.linters[name]) == "table" then
           lint.linters[name] = vim.tbl_deep_extend("force", lint.linters[name], linter)
         else
           lint.linters[name] = linter
@@ -40,11 +41,18 @@ return {
       end
 
       function M.lint()
-        local names = lint.linters_by_ft[vim.bo.filetype] or {}
+        local names = lint._resolve_linter_by_ft(vim.bo.filetype)
+        if #names == 0 then
+          vim.list_extend(names, lint.linters_by_ft["_"] or {})
+        end
+        vim.list_extend(names, lint.linters_by_ft["*"] or {})
         local ctx = { filename = vim.api.nvim_buf_get_name(0) }
         ctx.dirname = vim.fn.fnamemodify(ctx.filename, ":h")
         names = vim.tbl_filter(function(name)
           local linter = lint.linters[name]
+          if not linter then
+            LazyUtil.warn("Linter not found: " .. name, { title = "nvim-lint" })
+          end
           return linter and not (type(linter) == "table" and linter.condition and not linter.condition(ctx))
         end, names)
 
