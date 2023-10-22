d9d38c79c97ed9dfba9c64a574217e4fc047d1dc
Sho Sakakibara <sho@sakakibara.dev>
2023-10-22 23:20:33 +0900

    Show lsp server name in statusline

--- dot_config/nvim/lua/plugins/heirline/components/lsp.lua
+++ dot_config/nvim/lua/plugins/heirline/components/lsp.lua
@@ -4,7 +4,13 @@ local icons = require("config.icons")
 return {
   condition = conditions.lsp_attached,
   update = { "LspAttach", "LspDetach" },
-  provider = icons.status.Lsp .. "LSP" .. " ",
+  provider = function()
+    local names = {}
+    for _, server in pairs(vim.lsp.get_active_clients({ bufnr = 0 })) do
+      table.insert(names, server.name)
+    end
+    return " " .. icons.status.Lsp .. table.concat(names, " ") .. " "
+  end,
   hl = { fg = "green", bold = true },
   on_click = {
     name = "heirline_LSP",
