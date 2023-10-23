f917fc9b7f2970f5f17c2ba83448f05019a51311
Sho Sakakibara <sho@sakakibara.dev>
2023-10-23 15:35:31 +0900

    Disable lazy loading of heirline.nvim

--- dot_config/nvim/lua/plugins/heirline/init.lua
+++ dot_config/nvim/lua/plugins/heirline/init.lua
@@ -1,18 +1,8 @@
 return {
   {
     "rebelot/heirline.nvim",
-    event = "BufEnter",
-    init = function()
-      vim.g.heirline_laststatus = vim.o.laststatus
-      if vim.fn.argc(-1) > 0 then
-        vim.o.statusline = " "
-      else
-        vim.o.laststatus = 0
-      end
-    end,
+    lazy = false,
     config = function()
-      vim.o.laststatus = vim.g.heirline_laststatus
-
       local conditions = require("heirline.conditions")
       local utils = require("heirline.utils")
 
