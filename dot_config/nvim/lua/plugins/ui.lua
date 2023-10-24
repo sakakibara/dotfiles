1b064b7ce571ef10fea1645b21d9cf10d0906bd0
Sho Sakakibara <sho@sakakibara.dev>
2023-10-24 15:29:22 +0900

    Add mini.files

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -475,4 +475,75 @@ return {
       require("neo-tree").setup(opts)
     end,
   },
+
+  {
+    "echasnovski/mini.files",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = "VeryLazy",
+    init = function()
+      if vim.fn.argc(-1) == 1 then
+        if require("util.path").is_dir(tostring(vim.fn.argv(0))) then
+          require("mini.files")
+        end
+      end
+    end,
+    opts = {
+      windows = {
+        preview = false,
+        width_focus = 30,
+        width_preview = 30,
+      },
+      options = {
+        use_as_default_explorer = false,
+      },
+    },
+    keys = {
+      {
+        "<leader>fm",
+        function()
+          require("mini.files").open(require("util.path").get_current_file_path(), true)
+        end,
+        desc = "Open mini files",
+      },
+      {
+        "<leader>fM",
+        function()
+          require("mini.files").open(vim.loop.cwd(), true)
+        end,
+        desc = "Open mini files (cwd)",
+      },
+    },
+    config = function(_, opts)
+      require("mini.files").setup(opts)
+
+      local show_dotfiles = true
+      local filter_show = function(_)
+        return true
+      end
+      local filter_hide = function(fs_entry)
+        return not vim.startswith(fs_entry.name, ".")
+      end
+
+      local toggle_dotfiles = function()
+        show_dotfiles = not show_dotfiles
+        local new_filter = show_dotfiles and filter_show or filter_hide
+        require("mini.files").refresh({ content = { filter = new_filter } })
+      end
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "MiniFilesBufferCreate",
+        callback = function(args)
+          local buf_id = args.data.buf_id
+          vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf_id })
+        end,
+      })
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "MiniFilesActionRename",
+        callback = function(event)
+          require("lazyvim.util").lsp.on_rename(event.data.from, event.data.to)
+        end,
+      })
+    end,
+  },
 }
