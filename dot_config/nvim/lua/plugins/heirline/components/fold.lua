730d8a307af66e01c383ce8397c79c62ff715266
Sho Sakakibara <sho@sakakibara.dev>
2023-10-25 00:07:59 +0900

    Change heirline fold component

--- dot_config/nvim/lua/plugins/heirline/components/fold.lua
+++ dot_config/nvim/lua/plugins/heirline/components/fold.lua
@@ -1,16 +1,19 @@
 return {
+  static = {
+    fillchars = vim.opt.fillchars:get(),
+    ffi = require("util.ffi"),
+  },
   condition = function()
     return vim.opt.foldcolumn:get() ~= "0"
   end,
-  provider = function()
-    local fillchars = vim.opt.fillchars:get()
-    local foldopen = fillchars.foldopen
-    local foldclosed = fillchars.foldclose
-    local foldsep = fillchars.foldsep
-    local ffi = require("util.ffi")
-    local wp = ffi.C.find_window_by_handle(0, ffi.new("Error"))
-    local width = ffi.C.compute_foldcolumn(wp, 0)
-    local foldinfo = width > 0 and ffi.C.fold_info(wp, vim.v.lnum) or { start = 0, level = 0, llevel = 0, lines = 0 }
+  provider = function(self)
+    local foldopen = self.fillchars.foldopen
+    local foldclosed = self.fillchars.foldclose
+    local foldsep = self.fillchars.foldsep
+    local wp = self.ffi.C.find_window_by_handle(0, self.ffi.new("Error"))
+    local width = self.ffi.C.compute_foldcolumn(wp, 0)
+    local foldinfo = width > 0 and self.ffi.C.fold_info(wp, vim.v.lnum)
+      or { start = 0, level = 0, llevel = 0, lines = 0 }
     local str = ""
     if width ~= 0 then
       str = vim.v.relnum > 0 and "%#FoldColumn#" or "%#CursorLineFold#"
