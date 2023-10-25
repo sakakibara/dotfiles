61f32d6a1de6f19a2087309761e47c0828cb9080
Sho Sakakibara <sho@sakakibara.dev>
2023-10-25 14:13:47 +0900

    Change special statusline appearance

--- dot_config/nvim/lua/plugins/heirline/statusline/special.lua
+++ dot_config/nvim/lua/plugins/heirline/statusline/special.lua
@@ -1,12 +1,19 @@
 local conditions = require("heirline.conditions")
+local icons = require("config.icons")
 
 return {
   condition = function()
     return conditions.buffer_matches({
-      buftype = { "nofile", "prompt", "help", "quickfix" },
+      buftype = { "nofile", "prompt", "help" },
     })
   end,
-  require("plugins.heirline.components.file.type"),
+  require("heirline.utils").surround({ icons.powerline.block, icons.powerline.slant_right }, "dark_red", {
+    hl = {
+      fg = "bright_fg",
+      force = true,
+    },
+    require("plugins.heirline.components.file.type"),
+  }),
   { provider = "%q" },
   require("plugins.heirline.components.space"),
   require("plugins.heirline.components.file.help_filename"),
