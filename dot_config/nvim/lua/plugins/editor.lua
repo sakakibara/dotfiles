8d226972f81e4aa2779a7feb07f722e64e3f979e
Sho Sakakibara <sho@sakakibara.dev>
2023-10-28 21:21:20 +0900

    Add confirmation to buffer remove mapping

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -65,7 +65,18 @@ return {
       {
         "<leader>bd",
         function()
-          require("mini.bufremove").delete(0, false)
+          local bd = require("mini.bufremove").delete
+          if vim.bo.modified then
+            local choice = vim.fn.confirm(("Save changes to %q?"):format(vim.fn.bufname()), "&Yes\n&No\n&Cancel")
+            if choice == 1 then
+              vim.cmd.write()
+              bd(0)
+            elseif choice == 2 then
+              bd(0, true)
+            end
+          else
+            bd(0)
+          end
         end,
         desc = "Delete buffer",
       },
