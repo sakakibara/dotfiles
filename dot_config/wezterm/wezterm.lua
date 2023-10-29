10c39eed2d2b760bf821fa17a50779b66a22cdc6
Sho Sakakibara <sho@sakakibara.dev>
2023-10-29 19:28:28 +0900

    Add wezterm config file

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,33 @@
+local wezterm = require("wezterm")
+local config = {}
+local launch_menu = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.color_scheme = "catppuccin-mocha"
+config.font = wezterm.font("Sarasa Term J Nerd Font")
+config.font_size = 14.0
+
+launch_menu = {
+  {
+    label = "Zsh",
+    args = { "Zsh", "--login" },
+  },
+  {
+    label = "Bash",
+    args = { "bash", "--login" },
+  },
+}
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+  table.insert(launch_menu, {
+    label = "PowerShell",
+    args = { "powershell.exe", "-NoLogo" },
+  })
+end
+
+config.launch_menu = launch_menu
+
+return config
