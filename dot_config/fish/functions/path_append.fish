7753c230a9cb5f985f3a4fbc90d74cd6e21234e7
Sho Sakakibara <sho@sakakibara.dev>
2023-10-29 11:18:28 +0900

    Remove obsolete path utility functions

--- dot_config/fish/functions/path_append.fish
+++
@@ -1,7 +0,0 @@
-function path_append
-    for i in $argv
-        if test -d $i; and not contains $i $PATH
-            set PATH $PATH $i
-        end
-    end
-end
--- dot_config/fish/functions/path_prepend.fish
+++
@@ -1,7 +0,0 @@
-function path_prepend
-    for i in $argv
-        if test -d $i; and not contains $i $PATH
-            set PATH $i $PATH
-        end
-    end
-end
