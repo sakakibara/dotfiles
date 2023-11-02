bc52ccaabd00b3b20b8652370b8a1cb9b0eac0c1
Sho Sakakibara <sho@sakakibara.dev>
2023-11-02 17:28:57 +0900

    Remove vb from omnisharp filetypes

--- dot_config/nvim/lua/plugins/lang/csharp.lua
+++ dot_config/nvim/lua/plugins/lang/csharp.lua
@@ -59,6 +59,7 @@ return {
           enable_roslyn_analyzers = true,
           organize_imports_on_format = true,
           enable_import_completion = true,
+          filetypes = { "cs" },
         },
       },
     },
