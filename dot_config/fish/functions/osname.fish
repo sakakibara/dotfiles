27b31accd6692ce360d7c762753d1679e216dfca
Sho Sakakibara <sho@sakakibara.dev>
2023-11-04 21:07:15 +0900

    Update osname function

--- dot_config/fish/functions/osname.fish
+++ dot_config/fish/functions/osname.fish
@@ -1,25 +1,24 @@
 function osname -d "Get the current OS name"
-    set -l os
+    set -l osname
     switch (uname)
         case Darwin
-            set os macos
+            set osname macos
         case CYGWIN'*'
-            set os cygwin
+            set osname cygwin
         case Linux
-            if test -f /etc/os-release
-                set os (awk -F= '$1=="ID" { print tolower($2) ;}' "/etc/os-release")
+            if test -f /etc/osname-release
+                set osname (awk -F= '$1=="ID" { print tolower($2) ;}' "/etc/os-release")
             else if command -v lsb_release &>/dev/null
-                set os (lsb_release -si)
+                set osname (lsb_release -si)
             else if test -f /etc/lsb-release
-                set os (awk -F= '$1=="DISTRIB_ID" { print tolower($2) ;}' "/etc/lsb-release")
+                set osname (awk -F= '$1=="DISTRIB_ID" { print tolower($2) ;}' "/etc/lsb-release")
             else if test -f /etc/debian_version
-                set os debian
+                set osname debian
             end
     end
-
-    if test -n "$os"
-        echo $os
-        return 0
+    if test "$argv[1]" = init
+        echo "set -gx OSNAME $osname"
+    else
+        echo $osname
     end
-    return 1
 end
