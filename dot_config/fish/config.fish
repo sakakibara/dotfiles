f80f56b900576e19024cb57b0ecbd6c7d93bcd41
Sho Sakakibara <sho@sakakibara.dev>
2023-11-05 17:34:25 +0900

    Add tmux command abbreviations for fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -140,6 +140,12 @@ if status --is-interactive
     abbr -a .... cd ../../..
     abbr -a ..... cd ../../../..
     abbr -a -- - cd -
+    abbr -a -- ta tmux attach -t
+    abbr -a -- tad tmux attach -d -t
+    abbr -a -- tkss tmux kill-session -t
+    abbr -a -- tksv tmux kill-server
+    abbr -a -- tl tmux list-sessions
+    abbr -a -- ts tmux new-session -s
 
     if test (command -v fdfind)
         abbr -a fd fdfind
