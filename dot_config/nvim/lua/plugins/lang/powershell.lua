3a1d97f1b2212155d3541e95aaa1315e6dd271cb
Sho Sakakibara <sho@sakakibara.dev>
2023-11-14 19:15:23 +0900

    Add powershell lang settings

---
+++ dot_config/nvim/lua/plugins/lang/powershell.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, { "powershell-editor-services" })
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = function()
+      return {
+        servers = {
+          powershell_es = {
+            bundle_path = vim.fn.stdpath("data") .. "/mason/packages/powershell-editor-services",
+          },
+        },
+      }
+    end,
+  },
+}
