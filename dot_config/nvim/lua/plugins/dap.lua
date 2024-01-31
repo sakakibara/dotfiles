c160c8fea96d6684256d254ecda40120a9b38c30
Sho Sakakibara <sho@sakakibara.dev>
2024-01-31 18:10:49 +0900

    Change dap ui to use new tab on attach

--- dot_config/nvim/lua/plugins/dap.lua
+++ dot_config/nvim/lua/plugins/dap.lua
@@ -5,13 +5,6 @@ return {
       "rcarriga/nvim-dap-ui",
       keys = {
         {
-          "<leader>ud",
-          function()
-            require("dapui").toggle({ reset = true })
-          end,
-          desc = "Toggle dap ui",
-        },
-        {
           "<leader>de",
           function()
             require("dapui").eval()
@@ -25,14 +18,45 @@ return {
         local dap = require("dap")
         local dapui = require("dapui")
         dapui.setup(opts)
+
+        local debug_win = nil
+        local debug_tab = nil
+        local debug_tabnr = nil
+
+        local function open_in_tab()
+          if debug_win and vim.api.nvim_win_is_valid(debug_win) then
+            vim.api.nvim_set_current_win(debug_win)
+            return
+          end
+
+          vim.cmd("tabedit %")
+          debug_win = vim.fn.win_getid()
+          debug_tab = vim.api.nvim_win_get_tabpage(debug_win)
+          debug_tabnr = vim.api.nvim_tabpage_get_number(debug_tab)
+
+          dapui.open()
+        end
+
+        local function close_tab()
+          dapui.close()
+
+          if debug_tab and vim.api.nvim_tabpage_is_valid(debug_tab) then
+            vim.api.nvim_exec("tabclose " .. debug_tabnr, false)
+          end
+
+          debug_win = nil
+          debug_tab = nil
+          debug_tabnr = nil
+        end
+
         dap.listeners.after.event_initialized["dapui_config"] = function()
-          dapui.open({})
+          open_in_tab()
         end
         dap.listeners.before.event_terminated["dapui_config"] = function()
-          dapui.close({})
+          close_tab()
         end
         dap.listeners.before.event_exited["dapui_config"] = function()
-          dapui.close({})
+          close_tab()
         end
       end,
     },
