c94c0843874a1b15d0e4e6edffa961a4a5c1d560
Sho Sakakibara <sho@sakakibara.dev>
2024-03-15 02:29:58 +0900

    Add space between number and fold in statuscolumn

--- dot_config/nvim/lua/plugins/heirline/statuscolumn/init.lua
+++ dot_config/nvim/lua/plugins/heirline/statuscolumn/init.lua
@@ -2,5 +2,6 @@ return {
   require("plugins.heirline.components.signs"),
   require("plugins.heirline.components.align"),
   require("plugins.heirline.components.number"),
+  require("plugins.heirline.components.space"),
   require("plugins.heirline.components.fold"),
 }
