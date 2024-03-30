e88309023c88fcc3c379828e5f278cb23bce7274
Sho Sakakibara <sho@sakakibara.dev>
2024-03-30 11:40:39 +0900

    Remove mkdnflow.nvim

--- dot_config/nvim/lua/plugins/lang/markdown.lua
+++ dot_config/nvim/lua/plugins/lang/markdown.lua
@@ -86,53 +86,6 @@ return {
   },
 
   {
-    "jakewvincent/mkdnflow.nvim",
-    opts = {
-      to_do = {
-        symbols = { " ", "-", "x" },
-        complete = "x",
-      },
-      mappings = {
-        MkdnEnter = { { "n", "v" }, "<CR>" },
-        MkdnTab = false,
-        MkdnSTab = false,
-        MkdnNextLink = { "n", "<Tab>" },
-        MkdnPrevLink = { "n", "<S-Tab>" },
-        MkdnNextHeading = { "n", "]]" },
-        MkdnPrevHeading = { "n", "[[" },
-        MkdnGoBack = { "n", "<BS>" },
-        MkdnGoForward = { "n", "<Del>" },
-        MkdnCreateLink = false,
-        MkdnCreateLinkFromClipboard = { { "n", "v" }, "<localleader>p" },
-        MkdnFollowLink = false,
-        MkdnDestroyLink = { "n", "<M-CR>" },
-        MkdnTagSpan = { "v", "<M-CR>" },
-        MkdnMoveSource = { "n", "<F2>" },
-        MkdnYankAnchorLink = { "n", "yaa" },
-        MkdnYankFileAnchorLink = { "n", "yfa" },
-        MkdnIncreaseHeading = { "n", "+" },
-        MkdnDecreaseHeading = { "n", "-" },
-        MkdnToggleToDo = { { "n", "v" }, "<C-Space>" },
-        MkdnNewListItem = false,
-        MkdnNewListItemBelowInsert = { "n", "o" },
-        MkdnNewListItemAboveInsert = { "n", "O" },
-        MkdnExtendList = false,
-        MkdnUpdateNumbering = { "n", "<localleader>nn" },
-        MkdnTableNextCell = { "i", "<Tab>" },
-        MkdnTablePrevCell = { "i", "<S-Tab>" },
-        MkdnTableNextRow = false,
-        MkdnTablePrevRow = { "i", "<M-CR>" },
-        MkdnTableNewRowBelow = { "n", "<localleader>ir" },
-        MkdnTableNewRowAbove = { "n", "<localleader>iR" },
-        MkdnTableNewColAfter = { "n", "<localleader>ic" },
-        MkdnTableNewColBefore = { "n", "<localleader>iC" },
-        MkdnFoldSection = { "n", "<localleader>f" },
-        MkdnUnfoldSection = { "n", "<localleader>F" },
-      },
-    },
-  },
-
-  {
     "nvim-telescope/telescope.nvim",
     keys = {
       {
