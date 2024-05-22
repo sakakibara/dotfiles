6a11f36efec92316fc281fb953d5d2c9c24c1e4e
Sho Sakakibara <sho@sakakibara.dev>
2024-05-22 22:49:09 +0900

    Update lazy file load event

--- dot_config/nvim/lua/util/plugin.lua
+++ dot_config/nvim/lua/util/plugin.lua
@@ -66,73 +66,32 @@ function M.load(name)
   end
 end
 
-M.use_lazy_file = true
 M.lazy_file_events = { "BufReadPost", "BufNewFile", "BufWritePre" }
 
 function M.lazy_file()
-  M.use_lazy_file = M.use_lazy_file and vim.fn.argc(-1) > 0
-
-  local LazyEvent = require("lazy.core.handler.event")
-
-  if M.use_lazy_file then
-    LazyEvent.mappings.LazyFile = { id = "LazyFile", event = "User", pattern = "LazyFile" }
-    LazyEvent.mappings["User LazyFile"] = LazyEvent.mappings.LazyFile
-  else
-    LazyEvent.mappings.LazyFile = { id = "LazyFile", event = M.lazy_file_events }
-    LazyEvent.mappings["User LazyFile"] = LazyEvent.mappings.LazyFile
-    return
-  end
-
-  local events = {}
-
-  local done = false
-  local function load()
-    if #events == 0 or done then
-      return
-    end
-
-    done = true
-    vim.api.nvim_del_augroup_by_name("lazy_file")
-
-    local skips = {}
-    for _, event in ipairs(events) do
-      local augroups = LazyEvent.get_augroups(event.event)
-      local groups = vim.tbl_filter(function(t)
-        return not vim.tbl_contains({ t }, "filetypedetect")
-      end, augroups)
-      skips[event.event] = skips[event.event] or groups
-    end
-
-    vim.api.nvim_exec_autocmds("User", { pattern = "LazyFile", modeline = false })
-    for _, event in ipairs(events) do
-      if vim.api.nvim_buf_is_valid(event.buf) then
-        LazyEvent.trigger({
-          event = event.event,
-          exclude = skips[event.event],
-          data = event.data,
-          buf = event.buf,
-        })
-        if vim.bo[event.buf].filetype then
-          LazyEvent.trigger({
-            event = "FileType",
-            buf = event.buf,
-          })
-        end
-      end
-    end
-    vim.api.nvim_exec_autocmds("CursorMoved", { modeline = false })
-    events = {}
-  end
-
-  load = vim.schedule_wrap(load)
-
-  vim.api.nvim_create_autocmd(M.lazy_file_events, {
-    group = vim.api.nvim_create_augroup("lazy_file", { clear = true }),
+  vim.api.nvim_create_autocmd("BufReadPost", {
+    once = true,
     callback = function(event)
-      table.insert(events, event)
-      load()
+      if vim.v.vim_did_enter == 1 then
+        return
+      end
+
+      local ft = vim.filetype.match({ buf = event.buf })
+      if ft then
+        local lang = vim.treesitter.language.get_lang(ft)
+        if not (lang and pcall(vim.treesitter.start, event.buf, lang)) then
+          vim.bo[event.buf].syntax = ft
+        end
+
+        vim.cmd([[redraw]])
+      end
     end,
   })
+
+  local Event = require("lazy.core.handler.event")
+
+  Event.mappings.LazyFile = { id = "LazyFile", event = M.lazy_file_events }
+  Event.mappings["User LazyFile"] = Event.mappings.LazyFile
 end
 
 function M.has(name)
