6fb67e31a65298b5b416596e6b8aa2fdaf9041ca
Sho Sakakibara <sho@sakakibara.dev>
2024-05-31 00:30:56 +0900

    Add rename file lsp mapping

--- dot_config/nvim/lua/plugins/lsp/keymaps.lua
+++ dot_config/nvim/lua/plugins/lsp/keymaps.lua
@@ -57,6 +57,13 @@ function M.get()
     { "<Leader>cc", vim.lsp.codelens.run, desc = "Run codelens", mode = { "n", "v" }, has = "codeLens" },
     { "<Leader>cC", vim.lsp.codelens.refresh, desc = "Refresh & display codelens", mode = { "n" }, has = "codeLens" },
     {
+      "<leader>cR",
+      ulsp.rename_file,
+      desc = "Rename file",
+      mode = { "n" },
+      has = { "workspace/didRenameFiles", "workspace/willRenameFiles" },
+    },
+    {
       "]]",
       function()
         ulsp.words.jump(vim.v.count1)
