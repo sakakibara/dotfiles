cfb7da3d5b4a8667fc350c5a87bbad79422467fd
Sho Sakakibara <sho@sakakibara.dev>
2024-06-03 10:32:04 +0900

    Add missing config file

---
+++ dot_config/nvim/lua/config/init.lua
@@ -0,0 +1,246 @@
+_G.Util = require("util")
+
+local M = {}
+
+Util.config = M
+
+---@class ConfigOptions
+local options = {
+  icons = {
+    dap = {
+      Breakpoint = " ",
+      BreakpointCondition = " ",
+      BreakpointRejected = { " ", "DiagnosticError" },
+      LogPoint = ".>",
+      Stopped = { "󰁕 ", "DiagnosticWarn", "DapStoppedLine" },
+    },
+    diagnostics = {
+      Error = " ",
+      Hint = " ",
+      Info = " ",
+      Warn = " ",
+    },
+    git = {
+      added = " ",
+      modified = " ",
+      removed = " ",
+    },
+    kinds = {
+      Array = " ",
+      Boolean = " ",
+      Class = " ",
+      Color = " ",
+      Constant = " ",
+      Constructor = " ",
+      Copilot = " ",
+      Enum = " ",
+      EnumMember = " ",
+      Event = " ",
+      Field = " ",
+      File = " ",
+      Folder = " ",
+      Function = " ",
+      Interface = " ",
+      Key = " ",
+      Keyword = " ",
+      Method = " ",
+      Module = " ",
+      Namespace = " ",
+      Null = " ",
+      Number = " ",
+      Object = " ",
+      Operator = " ",
+      Package = " ",
+      Property = " ",
+      Reference = " ",
+      Snippet = " ",
+      String = " ",
+      Struct = " ",
+      Text = " ",
+      TypeParameter = " ",
+      Unit = " ",
+      Value = " ",
+      Variable = " ",
+    },
+    status = {
+      Lsp = " ",
+      Vim = " ",
+      Mode = " ",
+      Lock = " ",
+      Debug = " ",
+      Directory = " ",
+      DirectoryAlt = "󰉖 ",
+      Ellipsis = "…",
+      Separator = {
+        Breadcrumb = "",
+      },
+      Failure = " ",
+      Canceled = "󰜺 ",
+      Success = "󰄴 ",
+      Running = "󰑮 ",
+      FoldClose = "",
+      FoldOpen = "",
+      FoldSeparator = " ",
+    },
+    powerline = {
+      vertical_bar_thin = "│",
+      vertical_bar = "┃",
+      block = "█",
+      left = "",
+      left_filled = "",
+      right = "",
+      right_filled = "",
+      slant_left = "",
+      slant_left_thin = "",
+      slant_right = "",
+      slant_right_thin = "",
+      slant_left_inverse = "",
+      slant_left_inverse_thin = "",
+      slant_right_inverse = "",
+      slant_right_inverse_thin = "",
+      left_rounded = "",
+      left_rounded_thin = "",
+      right_rounded = "",
+      right_rounded_thin = "",
+      trapezoid_left = "",
+      trapezoid_right = "",
+      line_number = "",
+      column_number = "",
+    },
+  },
+  kind_filter = {
+    default = {
+      "Class",
+      "Constructor",
+      "Enum",
+      "Field",
+      "Function",
+      "Interface",
+      "Method",
+      "Module",
+      "Namespace",
+      "Package",
+      "Property",
+      "Struct",
+      "Trait",
+    },
+    markdown = false,
+    help = false,
+    lua = {
+      "Class",
+      "Constructor",
+      "Enum",
+      "Field",
+      "Function",
+      "Interface",
+      "Method",
+      "Module",
+      "Namespace",
+      "Property",
+      "Struct",
+      "Trait",
+    },
+  },
+}
+
+function M.setup(opts)
+  require("lazy").setup(opts)
+
+  local no_argc = vim.fn.argc(-1) == 0
+  if not no_argc then
+    M.load("autocmds")
+  end
+
+  vim.api.nvim_create_autocmd("User", {
+    group = vim.api.nvim_create_augroup("Core", { clear = true }),
+    pattern = "VeryLazy",
+    callback = function()
+      if no_argc then
+        M.load("autocmds")
+      end
+      M.load("keymaps")
+      Util.format.setup()
+      Util.root.setup()
+
+      vim.api.nvim_create_user_command("LazyHealth", function()
+        vim.cmd([[Lazy! load all]])
+        vim.cmd([[checkhealth]])
+      end, { desc = "Load all plugins and run :checkhealth" })
+    end,
+  })
+
+  Util.track("colorscheme")
+  Util.try(function()
+    vim.cmd.colorscheme(opts.install.colorscheme[1])
+  end, {
+    msg = "Failed to load the colorscheme",
+    on_error = function(msg)
+      Util.error(msg)
+      vim.cmd.colorscheme("colorscheme")
+    end,
+  })
+  Util.track()
+end
+
+function M.load(name)
+  local function _load(mod)
+    if require("lazy.core.cache").find(mod)[1] then
+      Util.try(function()
+        require(mod)
+      end, { msg = "Failed to load" .. mod })
+    end
+  end
+  _load("config." .. name)
+  if vim.bo.filetype == "lazy" then
+    vim.cmd([[do VimResized]])
+  end
+end
+
+function M.delay_notify()
+  local notifs = {}
+  local function temp(...)
+    table.insert(notifs, vim.F.pack_len(...))
+  end
+  local orig = vim.notify
+  vim.notify = temp
+  local timer = vim.uv.new_timer()
+  local check = assert(vim.uv.new_check())
+  local replay = function()
+    timer:stop()
+    check:stop()
+    if vim.notify == temp then
+      vim.notify = orig
+    end
+    vim.schedule(function()
+      for _, notif in ipairs(notifs) do
+        vim.notify(vim.F.unpack_len(notif))
+      end
+    end)
+  end
+  check:start(function()
+    if vim.notify ~= temp then
+      replay()
+    end
+  end)
+  timer:start(500, 0, replay)
+end
+
+M.inited = false
+function M.init()
+  if M.inited then
+    return
+  end
+  M.inited = true
+  M.delay_notify()
+  M.load("options")
+  Util.plugin.setup()
+end
+
+setmetatable(M, {
+  __index = function(_, key)
+    ---@cast options ConfigOptions
+    return options[key]
+  end,
+})
+
+return M
