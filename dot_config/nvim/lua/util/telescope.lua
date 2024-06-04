c219312ee3ad5dbf4e12a980af660fa679a3d48c
Sho Sakakibara <sho@sakakibara.dev>
2024-06-04 10:36:24 +0900

    Remove unused mapping from telescope

--- dot_config/nvim/lua/util/telescope.lua
+++ dot_config/nvim/lua/util/telescope.lua
@@ -23,19 +23,6 @@ function M.run(picker, opts)
       end
     end
 
-    if opts.cwd and opts.cwd ~= vim.uv.cwd() then
-      local function open_cwd_dir()
-        local action_state = require("telescope.actions.state")
-        local line = action_state.get_current_line()
-        M.run(params.builtin, vim.tbl_deep_extend("force", {}, params.opts or {}, { cwd = false, default_text = line }))()
-      end
-      ---@diagnostic disable-next-line: inject-field
-      opts.attach_mappings = function(_, map)
-        map("i", "<a-c>", open_cwd_dir, { desc = "Open cwd Directory" })
-        return true
-      end
-    end
-
     local builtin = require("telescope.builtin")[picker]
     if builtin then
       builtin(opts)
