b6f0b3dcc49ba695143dc45cb04140679506d4c5
Sho Sakakibara <sho@sakakibara.dev>
2024-06-13 00:55:51 +0900

    Add git settings

---
+++ dot_config/nvim/lua/plugins/lang/git.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "git_config", "gitcommit", "git_rebase", "gitignore", "gitattributes" } },
+  },
+
+  {
+    "nvim-cmp",
+    dependencies = {
+      { "petertriho/cmp-git", opts = {} },
+    },
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "git" })
+    end,
+  },
+}
