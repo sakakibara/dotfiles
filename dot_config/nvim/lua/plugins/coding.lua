930266c53d252353d070fa8291fc089771913b12
Sho Sakakibara <sho@sakakibara.dev>
2024-07-09 21:56:29 +0900

    Move codeium to extras

--- dot_config/nvim/lua/plugins/coding.lua
+++ dot_config/nvim/lua/plugins/coding.lua
@@ -395,25 +395,6 @@ return {
   },
 
   {
-    "nvim-cmp",
-    dependencies = {
-      {
-        "Exafunction/codeium.nvim",
-        cmd = "Codeium",
-        build = ":Codeium Auth",
-        opts = {},
-      },
-    },
-    opts = function(_, opts)
-      table.insert(opts.sources, 1, {
-        name = "codeium",
-        group_index = 1,
-        priority = 100,
-      })
-    end,
-  },
-
-  {
     "folke/lazydev.nvim",
     ft = "lua",
     opts = function()
---
+++ dot_config/nvim/lua/plugins/extras/coding/codeium.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "nvim-cmp",
+    dependencies = {
+      {
+        "Exafunction/codeium.nvim",
+        cmd = "Codeium",
+        build = ":Codeium Auth",
+        opts = {},
+      },
+    },
+    opts = function(_, opts)
+      table.insert(opts.sources, 1, {
+        name = "codeium",
+        group_index = 1,
+        priority = 100,
+      })
+    end,
+  },
+}
