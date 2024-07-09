0aa122c3f629ec9ac4de05ed5ffb60233344fe8e
Sho Sakakibara <sho@sakakibara.dev>
2024-07-09 21:23:22 +0900

    Change load all checkhealth command

--- dot_config/nvim/lua/config/init.lua
+++ dot_config/nvim/lua/config/init.lua
@@ -186,7 +186,7 @@ function M.setup(opts)
         Util.extras.show()
       end, { desc = "Manage extras" })
 
-      vim.api.nvim_create_user_command("LazyHealth", function()
+      vim.api.nvim_create_user_command("CheckHealth", function()
         vim.cmd([[Lazy! load all]])
         vim.cmd([[checkhealth]])
       end, { desc = "Load all plugins and run :checkhealth" })
