edb1caf5bc6f40bde74077b2a7576d0c4e874c1a
Sho Sakakibara <sho@sakakibara.dev>
2024-07-10 23:16:22 +0900

    Change statuscolumn git signs

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -295,12 +295,12 @@ return {
     event = "LazyFile",
     opts = {
       signs = {
-        add = { text = "▎" },
-        change = { text = "▎" },
+        add = { text = "┃" },
+        change = { text = "┃" },
         delete = { text = "_" },
         topdelete = { text = "‾" },
-        changedelete = { text = "▎" },
-        untracked = { text = "▎" },
+        changedelete = { text = "┃" },
+        untracked = { text = "┃" },
       },
       on_attach = function(buffer)
         local gs = package.loaded.gitsigns
--- dot_config/nvim/lua/plugins/heirline/statuscolumn/init.lua
+++ dot_config/nvim/lua/plugins/heirline/statuscolumn/init.lua
@@ -1,6 +1,6 @@
 return {
-  require("plugins.heirline.components.git_signs"),
   require("plugins.heirline.components.marks_or_other_signs"),
   require("plugins.heirline.components.number"),
+  require("plugins.heirline.components.git_signs"),
   require("plugins.heirline.components.fold"),
 }
