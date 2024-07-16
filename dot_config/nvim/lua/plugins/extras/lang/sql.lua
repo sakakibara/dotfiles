299841b48a772d82a45171f01a2d84dc559c31fa
Sho Sakakibara <sho@sakakibara.dev>
2024-07-16 09:32:51 +0900

    Add edgy.nvim

--- dot_config/nvim/lua/plugins/extras/lang/sql.lua
+++ dot_config/nvim/lua/plugins/extras/lang/sql.lua
@@ -55,29 +55,6 @@ return {
   },
 
   {
-    "folke/edgy.nvim",
-    optional = true,
-    opts = function(_, opts)
-      opts.right = opts.right or {}
-      table.insert(opts.right, {
-        title = "Database",
-        ft = "dbui",
-        pinned = true,
-        width = 0.3,
-        open = function()
-          vim.cmd("DBUI")
-        end,
-      })
-
-      opts.bottom = opts.bottom or {}
-      table.insert(opts.bottom, {
-        title = "DB Query Result",
-        ft = "dbout",
-      })
-    end,
-  },
-
-  {
     "williamboman/mason.nvim",
     opts = { ensure_installed = { "sqlfluff" } },
   },
