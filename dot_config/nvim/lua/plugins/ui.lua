88ca2a45daeaed23f16aa531cfa310a16987b6f1
Sho Sakakibara <sho@sakakibara.dev>
2024-07-19 10:29:34 +0900

    Remove unused edgy setting

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -647,17 +647,4 @@ return {
       })
     end,
   },
-
-  {
-    "nvim-telescope/telescope.nvim",
-    optional = true,
-    opts = {
-      defaults = {
-        get_selection_window = function()
-          require("edgy").goto_main()
-          return 0
-        end,
-      },
-    },
-  },
 }
