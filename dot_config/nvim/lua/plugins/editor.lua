b36327b173305ec6b7b9d2ff868802617ae71351
Sho Sakakibara <sho@sakakibara.dev>
2024-07-22 15:16:46 +0900

    Add grug-far.nvim

--- dot_config/nvim/lua/plugins/editor.lua
+++ dot_config/nvim/lua/plugins/editor.lua
@@ -363,6 +363,31 @@ return {
   },
 
   {
+    "MagicDuck/grug-far.nvim",
+    opts = { headerMaxWidth = 80 },
+    cmd = "GrugFar",
+    keys = {
+      {
+        "<leader>sr",
+        function()
+          local is_visual = vim.fn.mode():lower():find("v")
+          if is_visual then
+            vim.cmd([[normal! v]])
+          end
+          local grug = require("grug-far")
+          local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+          local filesFilter = ext and ext ~= "" and "*." .. ext or nil;
+          (is_visual and grug.with_visual_selection or grug.grug_far)({
+            prefills = { filesFilter = filesFilter },
+          })
+        end,
+        mode = { "n", "v" },
+        desc = "Search and replace",
+      },
+    },
+  },
+
+  {
     "stefandtw/quickfix-reflector.vim",
     ft = "qf",
     init = function()
