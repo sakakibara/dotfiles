ebf9d1c9a40a40d71a4b256155e8f1b5d6bbccf5
Sho Sakakibara <sho@sakakibara.dev>
2024-07-23 17:27:55 +0900

    Change quickfix statusline

--- dot_config/nvim/lua/plugins/heirline/statusline/quickfix.lua
+++ dot_config/nvim/lua/plugins/heirline/statusline/quickfix.lua
@@ -6,13 +6,20 @@ return {
       buftype = { "quickfix" },
     })
   end,
-  require("heirline.utils").surround({ Util.config.icons.powerline.block, Util.config.icons.powerline.block }, "gray", {
-    hl = {
-      fg = false,
-      force = true,
-    },
-    require("plugins.heirline.components.quickfix.label"),
-  }),
+  require("plugins.heirline.components.vi_mode"),
+  require("plugins.heirline.components.macro_rec"),
+  require("plugins.heirline.components.space"),
+  require("heirline.utils").surround(
+    { Util.config.icons.powerline.block, Util.config.icons.powerline.block },
+    "gray",
+    {
+      hl = {
+        fg = false,
+        force = true,
+      },
+      require("plugins.heirline.components.quickfix.label"),
+    }
+  ),
   require("plugins.heirline.components.space"),
   require("plugins.heirline.components.quickfix.title"),
   require("plugins.heirline.components.align"),
