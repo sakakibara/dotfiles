d1bbcdfed729dc57e35822573a88c8924f979a24
Sho Sakakibara <sho@sakakibara.dev>
2024-08-09 11:54:37 +0900

    Update nvim-cmp settings

--- dot_config/nvim/lua/plugins/coding.lua
+++ dot_config/nvim/lua/plugins/coding.lua
@@ -127,6 +127,7 @@ return {
       vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
       local cmp = require("cmp")
       local defaults = require("cmp.config.default")()
+      local auto_select = true
       return {
         completion = {
           completeopt = "menu,menuone,noinsert",
@@ -136,14 +137,14 @@ return {
             require("luasnip").lsp_expand(args.body)
           end,
         },
+        preselect = auto_select and cmp.PreselectMode.Item or cmp.PreselectMode.None,
         mapping = cmp.mapping.preset.insert({
-          ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-          ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
           ["<C-b>"] = cmp.mapping.scroll_docs(-4),
           ["<C-f>"] = cmp.mapping.scroll_docs(4),
           ["<C-Space>"] = cmp.mapping.complete(),
           ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = Util.cmp.confirm(),
+          ["<CR>"] = Util.cmp.confirm({ select = auto_select }),
+          ["<C-y>"] = Util.cmp.confirm({ select = true }),
           ["<S-CR>"] = Util.cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace }),
           ["<C-CR>"] = function(fallback)
             cmp.abort()
@@ -160,6 +161,18 @@ return {
             if icons[item.kind] then
               item.kind = icons[item.kind] .. item.kind
             end
+
+            local widths = {
+              abbr = 40,
+              menu = 30,
+            }
+
+            for key, width in pairs(widths) do
+              if item[key] and vim.fn.strdisplaywidth(item[key]) > width then
+                item[key] = vim.fn.strcharpart(item[key], 0, width - 1) .. "…"
+              end
+            end
+
             return item
           end,
         },
