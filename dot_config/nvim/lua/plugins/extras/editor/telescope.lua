ff924eae298055401f30243f391b762b97a1235d
Sho Sakakibara <sho@sakakibara.dev>
2024-08-09 13:28:22 +0900

    Set telescope sorting strategy to ascending

--- dot_config/nvim/lua/plugins/extras/editor/telescope.lua
+++ dot_config/nvim/lua/plugins/extras/editor/telescope.lua
@@ -173,6 +173,7 @@ return {
         defaults = {
           prompt_prefix = " ",
           selection_caret = " ",
+          sorting_strategy = "ascending",
           layout_config = {
             prompt_position = "top",
           },
