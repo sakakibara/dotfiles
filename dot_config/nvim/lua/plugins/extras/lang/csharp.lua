8f31c5c729b60af1763d3d0a753a973c84346b9e
Sho Sakakibara <sho@sakakibara.dev>
2024-09-23 13:44:33 +0900

    Change gd function depending on picker

--- dot_config/nvim/lua/plugins/extras/lang/csharp.lua
+++ dot_config/nvim/lua/plugins/extras/lang/csharp.lua
@@ -43,8 +43,10 @@ return {
           keys = {
             {
               "gd",
-              function()
+              Util.plugin.has("telescope.nvim") and function()
                 require("omnisharp_extended").telescope_lsp_definitions()
+              end or function()
+                require("omnisharp_extended").lsp_definitions()
               end,
               desc = "Goto Definition",
             },
