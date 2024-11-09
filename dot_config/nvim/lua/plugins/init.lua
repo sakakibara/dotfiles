4a730b6d094507811f9561e05a55d3f6b69b8446
Sho Sakakibara <sho@sakakibara.dev>
2024-11-09 16:29:39 +0900

    Add snacks.nvim

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -2,4 +2,23 @@ require("config").init()
 
 return {
   { "folke/lazy.nvim", version = "*" },
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = function()
+      return {
+        notifier = { enabled = not Util.plugin.has("noice.nvim") },
+      }
+    end,
+    keys = {
+      {
+        "<Leader>un",
+        function()
+          Snacks.notifier.hide()
+        end,
+        desc = "Dismiss all notifications",
+      },
+    },
+  },
 }
