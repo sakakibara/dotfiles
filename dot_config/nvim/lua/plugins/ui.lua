d650c1e9afdce92446baa922123a97b0642c721a
Sho Sakakibara <sho@sakakibara.dev>
2024-11-09 16:29:22 +0900

    Remove nvim-notify

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -214,44 +214,6 @@ return {
   },
 
   {
-    "rcarriga/nvim-notify",
-    keys = {
-      {
-        "<Leader>un",
-        function()
-          require("notify").dismiss({ silent = true, pending = true })
-        end,
-        desc = "Delete all notifications",
-      },
-    },
-    init = function()
-      local banned_messages = { "No information available" }
-      if not Util.plugin.has("noice.nvim") then
-        Util.plugin.on_very_lazy(function()
-          ---@diagnostic disable-next-line: duplicate-set-field
-          vim.notify = function(msg, ...)
-            for _, banned in ipairs(banned_messages) do
-              if msg == banned then
-                return
-              end
-            end
-            return require("notify")(msg, ...)
-          end
-        end)
-      end
-    end,
-    opts = {
-      timeout = 3000,
-      max_height = function()
-        return math.floor(vim.o.lines * 0.75)
-      end,
-      max_width = function()
-        return math.floor(vim.o.columns * 0.75)
-      end,
-    },
-  },
-
-  {
     "j-hui/fidget.nvim",
     enabled = function()
       return not Util.plugin.has("noice.nvim")
