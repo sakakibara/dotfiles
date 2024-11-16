5ea87f34e1a7c8f6ac2faa221410cd7f382e9425
Sho Sakakibara <sho@sakakibara.dev>
2024-11-16 16:12:09 +0900

    Update codeium settings

--- dot_config/nvim/lua/plugins/extras/ai/codeium.lua
+++ dot_config/nvim/lua/plugins/extras/ai/codeium.lua
@@ -1,14 +1,37 @@
 return {
   {
-    "nvim-cmp",
-    dependencies = {
-      {
-        "Exafunction/codeium.nvim",
-        cmd = "Codeium",
-        build = ":Codeium Auth",
-        opts = {},
+    "Exafunction/codeium.nvim",
+    cmd = "Codeium",
+    build = ":Codeium Auth",
+    opts = {
+      enable_cmp_source = true,
+      virtual_text = {
+        key_bindings = {
+          accept = false,
+          next = "<M-]>",
+          prev = "<M-[>",
+        },
       },
     },
+  },
+
+  {
+    "Exafunction/codeium.nvim",
+    opts = function()
+      Util.cmp.actions.ai_accept = function()
+        if require("codeium.virtual_text").get_current_completion_item() then
+          Util.keymaps.create_undo()
+          vim.api.nvim_input(require("codeium.virtual_text").accept())
+          return true
+        end
+      end
+    end,
+  },
+
+  {
+    "nvim-cmp",
+    optional = true,
+    dependencies = { "codeium.nvim" },
     opts = function(_, opts)
       table.insert(opts.sources, 1, {
         name = "codeium",
@@ -16,5 +39,19 @@ return {
         priority = 100,
       })
     end,
+  },
+
+  {
+    "saghen/blink.cmp",
+    optional = true,
+    opts = {
+      sources = {
+        compat = { "codeium" },
+      },
+    },
+    dependencies = {
+      "codeium.nvim",
+      "saghen/blink.compat",
+    },
   },
 }
