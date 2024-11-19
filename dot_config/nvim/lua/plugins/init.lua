18fcf2635bc43263547bacba05eb37d4bd53bf04
Sho Sakakibara <sho@sakakibara.dev>
2024-11-20 03:04:57 +0900

    Move opts below keys

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -6,15 +6,6 @@ return {
     "folke/snacks.nvim",
     priority = 1000,
     lazy = false,
-    opts = function()
-      return {
-        notifier = { enabled = true },
-        quickfile = { enabled = true },
-        bigfile = { enabled = true },
-        words = { enabled = true },
-        statuscolumn = { enabled = true, folds = { open = true } },
-      }
-    end,
     keys = {
       {
         "<Leader>un",
@@ -24,5 +15,14 @@ return {
         desc = "Dismiss all notifications",
       },
     },
+    opts = function()
+      return {
+        notifier = { enabled = true },
+        quickfile = { enabled = true },
+        bigfile = { enabled = true },
+        words = { enabled = true },
+        statuscolumn = { enabled = true, folds = { open = true } },
+      }
+    end,
   },
 }
