65a2b7d048ca5d38bcd915979603f375a6df49e3
Sho Sakakibara <sho@sakakibara.dev>
2025-02-07 20:30:23 +0900

    Add markdown filetype condition for render-markdown.nvim

--- dot_config/nvim/lua/plugins/extras/lang/markdown.lua
+++ dot_config/nvim/lua/plugins/extras/lang/markdown.lua
@@ -81,6 +81,7 @@ return {
   {
     "MeanderingProgrammer/render-markdown.nvim",
     dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+    ft = { "markdown" },
     opts = {},
   },
 
