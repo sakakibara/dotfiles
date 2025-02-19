9b9e81cac2916f5ed03123676517173dc73a63a3
Sho Sakakibara <sho@sakakibara.dev>
2025-02-19 18:12:44 +0900

    Update codeium config

--- dot_config/nvim/lua/plugins/extras/ai/codeium.lua
+++ dot_config/nvim/lua/plugins/extras/ai/codeium.lua
@@ -2,10 +2,12 @@ return {
   {
     "Exafunction/codeium.nvim",
     cmd = "Codeium",
+    event = "InsertEnter",
     build = ":Codeium Auth",
     opts = {
-      enable_cmp_source = true,
+      enable_cmp_source = vim.g.ai_cmp,
       virtual_text = {
+        enabled = not vim.g.ai_cmp,
         key_bindings = {
           accept = false,
           next = "<M-]>",
@@ -28,30 +30,21 @@ return {
     end,
   },
 
-  {
-    "nvim-cmp",
-    optional = true,
-    dependencies = { "codeium.nvim" },
-    opts = function(_, opts)
-      table.insert(opts.sources, 1, {
-        name = "codeium",
-        group_index = 1,
-        priority = 100,
-      })
-    end,
-  },
-
-  {
+  vim.g.ai_cmp and {
     "saghen/blink.cmp",
     optional = true,
+    dependencies = { "codeium.nvim", "saghen/blink.compat" },
     opts = {
       sources = {
         compat = { "codeium" },
+        providers = {
+          codeium = {
+            kind = "Codeium",
+            score_offset = 100,
+            async = true,
+          },
+        },
       },
     },
-    dependencies = {
-      "codeium.nvim",
-      "saghen/blink.compat",
-    },
-  },
+  } or nil,
 }
