12ad38698e60257d3ba27fc7f72ec35da9374c28
Sho Sakakibara <sho@sakakibara.dev>
2025-03-05 20:08:46 +0900

    Add mise setup

---
+++ etc/bash/lib/mise.bash
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+import msg
+
+mise::install() {
+  msg::heading "Installing mise"
+  curl https://mise.run | sh
+}
+
+# Check if mise is installed and try to install it if it isn't
+mise::require() {
+  msg::heading "Checking if mise is installed"
+  if [[ ! "$(command -v mise)" ]]; then
+    msg::arrow "Mise is missing"
+    mise::install
+    # Exit if, for some reason, mise is not installed
+    if [[ ! "$(command -v mise)" ]]; then
+      msg::error "Mise installation has failed"
+      return 1
+    fi
+  fi
+  msg::success "Mise is installed"
+}
+
+mise::setup() {
+  msg::heading "Require mise"
+  # Check if mise is installed and try to install it if it isn't
+  mise::require || return 1
+}
