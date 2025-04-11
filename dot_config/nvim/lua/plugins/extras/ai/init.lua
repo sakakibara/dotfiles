a246cb8ae46c7b701025f4fccdca1b40d45578df
Sho Sakakibara <sho@sakakibara.dev>
2025-04-11 18:34:33 +0900

    Remove codeium.nvim

--- dot_config/nvim/lua/plugins/extras/ai/init.lua
+++ dot_config/nvim/lua/plugins/extras/ai/init.lua
@@ -2,12 +2,6 @@ local is_private = Util.system.role() == "private"
 
 return {
   {
-    import = "plugins.extras.ai.codeium",
-    enabled = function()
-      return is_private
-    end,
-  },
-  {
     import = "plugins.extras.ai.copilot",
     enabled = function()
       return not is_private
