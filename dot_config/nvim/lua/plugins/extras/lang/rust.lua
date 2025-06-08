2fd5dfdeba9f174a511eca2e653ae559fcd021b6
Sho Sakakibara <sho@sakakibara.dev>
2025-06-09 01:27:21 +0900

    Remove unnecessary ensure installed for rust

--- dot_config/nvim/lua/plugins/extras/lang/rust.lua
+++ dot_config/nvim/lua/plugins/extras/lang/rust.lua
@@ -27,7 +27,7 @@ return {
     optional = true,
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "codelldb", "rust-analyzer" })
+      vim.list_extend(opts.ensure_installed, { "codelldb" })
       if diagnostics == "bacon-ls" then
         vim.list_extend(opts.ensure_installed, { "bacon" })
       end
