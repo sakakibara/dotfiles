1ffa24f983fbf5236bcd94c950e41ab6c1e17855
Sho Sakakibara <sho@sakakibara.dev>
2026-08-31 23:45:43 +0900

    Detect PlantUML files so its syntax plugin can load at all

--- src/.config/nvim/lua/config/plugins/lang/plantuml.lua
+++ src/.config/nvim/lua/config/plugins/lang/plantuml.lua
@@ -1,3 +1,13 @@
+vim.filetype.add({
+  extension = {
+    pu = "plantuml",
+    uml = "plantuml",
+    plantuml = "plantuml",
+    puml = "plantuml",
+    iuml = "plantuml",
+  },
+})
+
 return Lib.lang.setup({
   plugins = {
     { "aklt/plantuml-syntax", ft = "plantuml" },
