32f726468470afd2272f62d0cbcb3452e89ad3f7
Sho Sakakibara <sho@sakakibara.dev>
2026-09-02 01:07:38 +0900

    Keep the tailwind filetype list off the startup path

--- src/.config/nvim/lua/config/plugins/lang/tailwind.lua
+++ src/.config/nvim/lua/config/plugins/lang/tailwind.lua
@@ -15,10 +15,7 @@ local default_filetypes = {
 }
 
 local function served(filetypes)
-  local ok, known = pcall(vim.filetype._get_known_filetypes)
-  return vim.tbl_filter(function(ft)
-    return not exclude[ft] and (not ok or known[ft])
-  end, filetypes)
+  return vim.tbl_filter(function(ft) return not exclude[ft] end, filetypes)
 end
 
 return Lib.lang.setup({
